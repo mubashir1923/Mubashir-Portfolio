c7ff6837c8fa61356a1f2b377579d78a46275448
Mubashir <gaminglifee0614@gmail.com>
2025-08-02 06:08:28 +0500

    v1.0.1

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-const cards = document.querySelectorAll('.card');
-cards.forEach((card, index) => {
+document.addEventListener("DOMContentLoaded", () => {
+  const cards = document.querySelectorAll('.card');
+  cards.forEach((card, index) => {
+    card.style.animation = `fadeInUp 0.5s ease forwards`;
     card.style.animationDelay = `${index * 100}ms`;
+  });
 });
